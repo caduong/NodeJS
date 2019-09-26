6127e9ec819748ae505712a1accf16eac1abddf8
caduong <anhduongtk97@gmail.com>
2019-09-26 17:51:26 +0700

    "update"

---
+++ APIExpessMongoDB/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+console.log("aaaaaaaaaaaaaaaaaaaaa")
+// Middlewares
+app.use(logger('dev'));
+
+// Routes
+app.get('/users', (req, res, next) => {
+    res.status(200).json({
+        message: 'You requested index page'
+    })
+});
+
+app.post('/user', (req, res, next) => {
+    res.status(200).json({
+        message: 'You requested index page'
+    })
+});
+
+// // Catch 404 Errors and forward them to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Error handler function
+app.use((err, req, res, next) => {
+    const error = app.get('env') === 'development' ? err : {};
+    const status = err.status || 500;
+
+    // Respond to client
+    res.status(status).json({
+        error: {
+            message: error.message
+        }
+    });
+
+
+    // Respond to ourselves
+    console.error(err);
+});
+// Start the server
+const port = app.get('port') || 7000;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
