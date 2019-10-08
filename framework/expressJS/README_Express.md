258e5e696c46d8337157415046c03a9ff40acf68
caduong <anhduongtk97@gmail.com>
2019-10-08 11:42:16 +0700

    update

--- framework/expressJS/README_Express.md
+++
@@ -1,4 +0,0 @@
-# Express farmework
-
-
-    
--- nodeJS_tutorial/OOP/app.js
+++
@@ -1,7 +0,0 @@
-const tutorial = require('./tutorial');
-
-//console.log(tutorial);
-
-console.log(tutorial.sum(10,50));
-console.log(tutorial.PI);
-console.log(new tutorial.SomeMathObject());
